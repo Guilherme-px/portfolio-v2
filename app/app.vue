6968ddee53833875bf4a801bbc23b506ee114b6c
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-02-05 00:00:05 -0300

    chore: create Nuxt project with base config

---
+++ app/app.vue
@@ -0,0 +1,34 @@
+<template>
+    <NuxtPage />
+</template>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+html,
+body {
+    overflow-x: hidden;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    scroll-behavior: smooth;
+}
+
+::-webkit-scrollbar {
+    width: 12px;
+}
+
+::-webkit-scrollbar-thumb {
+    background-color: #ffd500;
+    border-radius: 6px;
+    border: 2px solid #111;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background-color: #eab308;
+}
+</style>
