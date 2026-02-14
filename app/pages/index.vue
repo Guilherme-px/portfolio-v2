8a2fbe84faaad63c875025155e3a06447024118d
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-02-14 20:32:28 -0300

    feat(about): add about section to page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
     <main>
         <Hero />
+        <About />
     </main>
 </template>
 
 <script setup lang="ts">
+import About from "~/components/About.vue";
 import Hero from "~/components/Hero.vue";
 </script>
