e5e779bb6b65c28f82da8a306d494c86ec27c9c3
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-02-14 02:33:53 -0300

    feat: install and use nuxt-ui and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
-    devtools: { enabled: true },
-    modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+    devtools: { enabled: false },
+    modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+    css: ["~/assets/css/main.css"],
     ssr: false,
+    fonts: {
+        families: [
+            {
+                name: "JetBrains Mono",
+                weights: ["400", "700"],
+            },
+        ],
+    },
+    icon: {
+        collections: ["uil", "lucide"],
+    },
 });
