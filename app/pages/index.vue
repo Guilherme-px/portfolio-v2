655d4a45bea38a09817b2e5bf9c9104c7b717452
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-02-14 22:23:58 -0300

    feat(skills): add skills section on page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,10 +2,12 @@
     <main>
         <Hero />
         <About />
+        <Skills />
     </main>
 </template>
 
 <script setup lang="ts">
 import About from "~/components/About.vue";
 import Hero from "~/components/Hero.vue";
+import Skills from "~/components/Skills.vue";
 </script>
