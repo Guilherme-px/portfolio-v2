57ee958cd01cc5901e44150feb16aba80777110f
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-02-15 19:33:15 -0300

    feat(page): add footer to main page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,10 +4,12 @@
         <About />
         <Skills />
     </main>
+    <Footer />
 </template>
 
 <script setup lang="ts">
 import About from "~/components/About.vue";
 import Hero from "~/components/Hero.vue";
 import Skills from "~/components/Skills.vue";
+import Footer from "~/components/Footer.vue";
 </script>
