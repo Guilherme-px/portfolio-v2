587498e69671e0bbe98a480e76a00d2c2ee6a856
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-02-20 18:43:43 -0300

    feat(utils): add utils for icons list

---
+++ app/utils/iconsList.ts
@@ -0,0 +1,48 @@
+import serverlessImg from "@/assets/icons/serverless-icon.svg";
+
+export const rawSkills = [
+    { name: "JAVASCRIPT", icon: "devicon-javascript-plain", brand: "#F7DF1E" },
+    { name: "TYPESCRIPT", icon: "devicon-typescript-plain", brand: "#3178C6" },
+    { name: "C#", icon: "devicon-csharp-plain", brand: "#239120" },
+
+    { name: "REACT", icon: "devicon-react-plain", brand: "#61DAFB" },
+    { name: "VUE.JS", icon: "devicon-vuejs-plain", brand: "#42B883" },
+    {
+        name: "NODE.JS",
+        icon: "devicon-nodejs-plain-wordmark",
+        brand: "#339933",
+    },
+
+    { name: "MONGODB", icon: "devicon-mongodb-plain", brand: "#47A248" },
+    { name: "POSTGRESQL", icon: "devicon-postgresql-plain", brand: "#336791" },
+
+    { name: "NEXT.JS", icon: "devicon-nextjs-plain", brand: "#000000" },
+    {
+        name: "TAILWIND",
+        icon: "devicon-tailwindcss-original",
+        brand: "#06B6D4",
+    },
+
+    { name: "DOCKER", icon: "devicon-docker-plain", brand: "#2496ED" },
+    { name: "GIT", icon: "devicon-git-plain", brand: "#F05032" },
+    { name: "NUXT.JS", icon: "devicon-nuxtjs-plain", brand: "#00DC82" },
+
+    { name: "HTML5", icon: "devicon-html5-plain", brand: "#E34F26" },
+    { name: "Css3", icon: "devicon-css3-plain", brand: "#1572B6" },
+
+    { name: "EXPRESS", icon: "devicon-express-original", brand: "#111111" },
+    { name: "DOTNET", icon: "devicon-dotnetcore-plain", brand: "#512BD4" },
+    { name: "MYSQL", icon: "devicon-mysql-original", brand: "#4479A1" },
+
+    { name: "SERVERLESS", iconImg: serverlessImg, brand: "#FD5750" },
+
+    {
+        name: "AWS",
+        icon: "devicon-amazonwebservices-plain-wordmark",
+        brand: "#FF9900",
+    },
+
+    { name: "JEST", icon: "devicon-jest-plain", brand: "#C21325" },
+    { name: "VITEST", icon: "devicon-vitest-plain", brand: "#6E9F18" },
+    { name: "CYPRESS", icon: "devicon-cypressio-plain", brand: "#17202C" },
+];
